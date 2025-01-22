d13bc2c792a050d1bc95b15cae89985e2d7d51b4
prasanna-sir <142475862+prasanna-sir@users.noreply.github.com>
2025-01-22 15:38:07 +0000

    variables ko barema

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "prasanna@gmail.com"
+var accountPw= "1213"
+accountCity = "kathmandu"
+
+// accountId = 21212 not allowed
+
+accountEmail = "sas@jf.com"
+accountPw= "21"
+accountCity= "pkr"
+let accountState;
+console.log(accountId);
+console.table([accountEmail, accountId, accountPw, accountState , accountCity])
+
