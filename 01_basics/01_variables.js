088d6e9e091ce2f0229891484e8856fdabd88660
prasanna-sir <142475862+prasanna-sir@users.noreply.github.com>
2025-02-11 14:12:00 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,8 @@
 const accountId = 144553
 let accountEmail = "prasanna@gmail.com"
 var accountPw= "1213"
+/*prefer not to use var because of issue in block scope and functional scope
+*/
 accountCity = "kathmandu"
 
 // accountId = 21212 not allowed
