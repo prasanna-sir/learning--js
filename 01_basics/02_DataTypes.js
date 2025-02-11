8988499e8e0c20fdcfc2336b2058483ad811bcbc
prasanna-sir <142475862+prasanna-sir@users.noreply.github.com>
2025-02-11 14:11:09 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treats js code as newer version of js
+// alert(3+3) //doesnot work as we are using node js
+ let username = "prasanna" //string
+ let age = 18 // num
+ let isLoggedIn = true //boolean
+ // null => standalone value let state= null ; , jastai temp lai 0 vanda null deko ramro kina vane 0 is still temp
+ // undefined vaneko declare garna baki , eg. let state; variable declare ta vayo tara defined chaina
+ // symbol => for unique
+
+ //object =>
+    console.log(typeof "prasanna")
+//type of null is object 
+
