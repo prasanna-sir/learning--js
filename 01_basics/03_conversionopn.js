26bb4348aef1b83af5e295239dcfa5a4edb911a9
prasanna-sir <142475862+prasanna-sir@users.noreply.github.com>
2025-02-11 15:57:18 +0000

    conversion ko kura haru

---
+++ 01_basics/03_conversionopn.js
@@ -0,0 +1,16 @@
+let score="false"
+//console.log(typeof score)
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+// 33 =>33 number
+// 33abc => NaN - not a number 
+//boolean =>0/1
+let isloggedin = 0
+let valueInBoolean = Boolean(isloggedin)
+console.log(valueInBoolean)
+//console.log(isloggedin)
+
+let name = 33
+ let converttostr = String("name")
+ console.log(converttostr)
